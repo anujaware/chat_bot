00c518e7fcc2faca048276eab7e6fefba28d25e0
Anuja Ware <anuja@joshsoftware.com>
2017-07-12 10:57:26 +0530

    Spec model added for Category

--- app/models/chat_bot/category.rb
+++ app/models/chat_bot/category.rb
@@ -1,4 +1,7 @@
 module ChatBot
   class Category < ActiveRecord::Base
+
+    validates :name, presence: true, uniqueness: {case_sensitive: false}
+
   end
 end
--- spec/models/chat_bot/category_test.rb
+++ spec/models/chat_bot/category_test.rb
@@ -1,53 +1,59 @@
-require './test/test_helper'
+require 'rails_helper'
 
-module ChatBot
-  class CategoryTest < ActiveSupport::TestCase
-    should validate_presence_of :name
+RSpec.describe ChatBot::Category, type: :model do
+  Category = ChatBot::Category
 
-    def setup
-      @category = Category.new #categories(:one)
+  context "Validation" do
+    it { should validate_presence_of(:name) }
+
+    before do
+      @category = Category.new
     end
 
-    def test_save_fail_without_name
-      assert_not @category.save, "Name blank/nil"
-    end
+  end
+end
+=begin 
+  def setup
+    @category = Category.new #categories(:one)
+  end
 
-    def test_save_fail_for_duplicate_name
-      @category.name = 'Cat 1'
-      assert @category.save
-      category = Category.new name: 'Cat 1' #categories(:two)
-      assert_not category.save
-    end
+  def test_save_fail_for_duplicate_name
+    @category.name = 'Cat 1'
+    assert @category.save
+    category = Category.new name: 'Cat 1' #categories(:two)
+    assert_not category.save
+  end
 
-    def test_save_fail_for_incase_sensitive_duplicate_name
-      category = Category.new name: 'Cat 1'
-      assert category.save
-      category = Category.new name: 'cat 1'
-      assert_not category.save
-    end
-    
-    def test_strip_name
-      category = Category.new name: "   Cat   \n \t 1  \n "
-      assert category.save
-      category.reload
-      assert_equal category.name, 'Cat 1'
-    end
+  def test_save_fail_for_incase_sensitive_duplicate_name
+    category = Category.new name: 'Cat 1'
+    assert category.save
+    category = Category.new name: 'cat 1'
+    assert_not category.save
+  end
 
-    def test_capitaliez_name
-      category = Category.create name: 'applIcatioN inTRoductiON'
-      assert_equal category.reload.name, 'Application introduction'
-    end
+  def test_strip_name
+    category = Category.new name: "   Cat   \n \t 1  \n "
+    assert category.save
+    category.reload
+    assert_equal category.name, 'Cat 1'
+  end
 
-    def test_sub_categories
-      #assert_must Category, have_many(:sub_categories)
+  def test_capitaliez_name
+    category = Category.create name: 'applIcatioN inTRoductiON'
+    assert_equal category.reload.name, 'Application introduction'
+  end
+
+  def test_sub_categories
+    #assert_must Category, have_many(:sub_categories)
 =begin
 ### TODO: ERROR - Testing association
 ChatBot::CategoryTest#test_sub_categories:
 NoMethodError: undefined method `matches?' for ChatBot::Category:Class
     test/models/chat_bot/category_test.rb:35:in `test_sub_categories'
-=end
-    end
+ =end
+  end
 
+=begin    
     describe 'Method' do
       context '#find_or_create' do
         context 'should create single entry for' do
@@ -60,5 +66,5 @@ NoMethodError: undefined method `matches?' for ChatBot::Category:Class
         end
       end
     end
-  end
-end
+ =end    
+=end
